2f6fab0e1bd0ff55bf192e1a96dc852d38d3cd1c
1m1 <imi@1m1.io>
2026-06-02 14:18:18 -0500

    2.0.0

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # TOGOmega
+
+## Install julia
+
+https://julialang.org/downloads
+
+## Install
+
+julia -m 
+
+mkdir TheoryOfGod && cd TheoryOfGod
+
+## Run
+
+julia -e 'using Pkg;Pkg.add(url="https://github.com/1m1-github/TOGInstall.git");using TOGInstall;TOGInstall.awaken();Pkg.rm("TOGInstall")'
--- src/TOGOmega.jl
+++ src/TOGOmega.jl
@@ -3,43 +3,60 @@ module TOGOmega
 const T = Float32
 export T
 
-using Pkg
+using Pkg, RemoteREPL
 using TOG: 𝕋, t
-using TOGZMQServer, TOGREPL, TOGCommunicationServer, TOGInstall
+using TOGZMQServer, TOGCommunicationServer
 
 const Ω = 𝕋()
 
-const ROUTERLOCATION = "ipc://$(pwd())/router.ipc" # change to tcp if on separate machines
-const PUBLOCATION = "ipc://$(pwd())/pub.ipc" # change to tcp if on separate machines
-const TOGLOCATION = "ipc://$(pwd())/tog.ipc" # change to tcp if on separate machines
-
-function awaken(gods)
-    Pkg.update()
-    TOGZMQServer.awaken(router=ROUTERLOCATION,pub=PUBLOCATION,tog=TOGLOCATION, ω=Ω)
-    TOGCommunicationServer.awaken(router=ROUTERLOCATION, pub=PUBLOCATION)
-    [TOGInstall.awakengod(name=god, router=ROUTERLOCATION, pub=PUBLOCATION, tog=TOGLOCATION, replport=TOGInstall.openport()) for god = gods]
-    TOGREPL.awaken(true)
+# function awaken(gods)
+function awaken(;router::String, pub::String, tog::String, replport::Integer)
+    # Pkg.update()
+    # write(joinpath(".tog", "pid"), getpid())
+    @async serve_repl(replport)
+    TOGZMQServer.awaken(router=router,pub=pub,tog=tog, ω=Ω)
+    TOGCommunicationServer.awaken(router=router, pub=pub)
+    # [TOGInstall.awakengod(name=god, router=ROUTERLOCATION, pub=PUBLOCATION, tog=TOGLOCATION, replport=TOGInstall.openport()) for god = gods]
+    # TOGREPL.awaken(true)
 end
 
+# function parse_commandline()
+#     s = ArgParseSettings()
+#     @add_arg_table s begin
+#         "--init"
+#         action = :store_true
+#         "--update"
+#         action = :store_true
+#         "path"
+#         arg_type = String
+#         default = "."
+#     end
+#     return parse_args(s)
+# end
+
 # function (@main)(ARGS)
-#     # Pkg.update()
-#     # manifest = joinpath(dirname(Pkg.project().path), "Manifest.toml")
-#     # prehash = hash(read(manifest))
-#     # Pkg.update()
-#     # posthash = hash(read(manifest))
-#     # updated = prehash != posthash
-#     # if updated
-#     #     run(`tog `)
-#     #     exit(0)
-#     # end
-#     # DEPOT_PATH[1]=joinpath(pwd(),".loopos")
+# #     # Pkg.update()
+# #     # manifest = joinpath(dirname(Pkg.project().path), "Manifest.toml")
+# #     # prehash = hash(read(manifest))
+# #     # Pkg.update()
+# #     # posthash = hash(read(manifest))
+# #     # updated = prehash != posthash
+# #     # if updated
+# #     #     run(`tog `)
+# #     #     exit(0)
+# #     # end
+# #     # DEPOT_PATH[1]=joinpath(pwd(),".loopos")
 #     config = parse_commandline()
+#     if config["init"]
+
+#     end
 #     if config["update"]
 #         Pkg.update()
-#         Pkg.Apps.update("tog")
+#         # Pkg.Apps.update("tog")
 #         return
 #     end
-#     TOGInstall.awaken()
+#     awaken(path=ARGS[1])
+# #     TOGInstall.awaken()
 # end
 
 end
