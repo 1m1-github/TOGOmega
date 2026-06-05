856f647fce6fbacfb47a7b396c2721416558ad1e
1m1 <imi@1m1.io>
2026-06-05 00:42:26 -0500

    23.0.0

--- src/TOGOmega.jl
+++ src/TOGOmega.jl
@@ -10,18 +10,18 @@ using TOGZMQServer, TOGCommunicationServer, TOGAwaken
 const Ωpath = joinpath(TOGAwaken.TOGDIR, "Ω")
 const Ω = isfile(Ωpath) ? deserialize(Ωpath) : 𝕋()
 
-function awaken(; router=TOGAwaken.router(), pub=TOGAwaken.pub(), tog=TOGAwaken.tog())
-    TOGAwaken.isrunning() && error("TOGOmega is already running.")
-    TOGAwaken.writepid()
-    TOGZMQServer.awaken(router=router, pub=pub, tog=tog, ω=Ω)
-    TOGCommunicationServer.awaken(router=router, pub=pub)
-end
-
 function __init__()
     atexit(_ -> begin
         serialize(Ωpath, Ω)
         TOGAwaken.rmpid()
     end)
+end
+
+function awaken(; router=TOGAwaken.router(), pub=TOGAwaken.pub(), tog=TOGAwaken.tog())
+    TOGAwaken.isrunning() && error("TOGOmega is already running.")
+    TOGAwaken.writepid()
+    TOGZMQServer.awaken(router=router, pub=pub, tog=tog, ω=Ω)
+    TOGCommunicationServer.awaken(router=router, pub=pub)
 end
 
 end
