55ab00f3055e73007c23fcfda693604ef2098a7c
1m1 <imi@1m1.io>
2026-06-23 14:44:04 -0500

    36.0.0

--- src/TOGOmega.jl
+++ src/TOGOmega.jl
@@ -1,33 +1,28 @@
 module TOGOmega
 
-export t, T
-
 const T = Float64
 
 using Pkg, Serialization
 using TOG: 𝕋
-using TOGZMQServer, TOGCommunicationServer, TOGAwaken, TOGLogging
+using TOGCommunicationServer, TOGAwaken, TOGLogging, TOGObserveServer, TOGCreateServer
 using TOGColor, Colors # DEBUG?
 
 const Ωpath = joinpath(TOGAwaken.TOGDIR, "Ω")
 const Ω = isfile(Ωpath) ? deserialize(Ωpath) : 𝕋(T)
-import TOG: t
-t() = t(Ω)
 
-function __init__()
-    atexit(_ -> begin
-        # serialize(Ωpath, Ω)
-        TOGAwaken.rmpid()
-        TOGZMQServer.sleep()
-        TOGCommunicationServer.sleep()
-    end)
+__init__() = atexit(sleep)
+function sleep()
+    # serialize(Ωpath, Ω)
+    TOGAwaken.sleep()
+    TOGObserveServer.sleep()
+    TOGCreateServer.sleep()
+    TOGCommunicationServer.sleep()
 end
-
-function awaken(; router=TOGAwaken.router(), pub=TOGAwaken.pub(), tog=TOGAwaken.tog())
+function awaken(; router=TOGAwaken.router(), pub=TOGAwaken.pub(), togobserve=TOGAwaken.togobserve(), togcreate=TOGAwaken.togcreate())
     TOGLogging.awaken()
-    TOGAwaken.isrunning() && error("TOGOmega is already running.")
-    TOGAwaken.writepid()
-    TOGZMQServer.awaken(router=router, pub=pub, tog=tog, ω=Ω)
+    TOGAwaken.awaken()
+    TOGObserveServer.awaken(socketlocation=togobserve, ω=Ω)
+    TOGCreateServer.awaken(socketlocation=togcreate, ω=Ω)
     TOGCommunicationServer.awaken(router=router, pub=pub)
 end
 
